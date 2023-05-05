dabff33db3bf12fe58b271474474455bbf93e3cf
lorenzo <lorenzodimaio02@gmail.com>
2023-05-05 16:39:50 +0200

    versione con serviceSettingController

--- src/main/java/com/preventivoapp/appproject_preventivo/controller/quoteMainController.java
+++ src/main/java/com/preventivoapp/appproject_preventivo/controller/quoteMainController.java
@@ -129,4 +129,8 @@ public class quoteMainController {
             showNoElementSelected();
         }
     }
+
+    public void addServiceToList (Service service) {
+        serviceList.add(service);
+    }
 }
