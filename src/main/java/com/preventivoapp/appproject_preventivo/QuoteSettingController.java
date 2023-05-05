59a2d82235286b944573d9b766a4a478fd2b9c2e
dcfrenci2 <dcfrenci2@gmail.com>
2023-05-05 12:34:58 +0200

    QuoteSettingController modificato

--- src/main/java/com/preventivoapp/appproject_preventivo/QuoteSettingController.java
+++ src/main/java/com/preventivoapp/appproject_preventivo/QuoteSettingController.java
@@ -5,6 +5,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class QuoteSettingController {
     //NEW QUOTE -->
@@ -17,10 +18,36 @@ public class QuoteSettingController {
     @FXML private TableColumn<Service, String> newQuoteNameColumn;
     @FXML private TableColumn<Quote, Integer> newQuoteNumberColumn;
     @FXML private Button newQuotePreview;
-    @FXML private TableColumn<?, ?> newQuotePriceColumn;
-    @FXML private TableColumn<?, ?> newQuotePriceForToothColumn;
+    @FXML private TableColumn<Service, Double> newQuotePriceColumn;
+    @FXML private TableColumn<Service, Double> newQuotePriceForToothColumn;
     @FXML private Button newQuoteRemove;
     @FXML private Button newQuoteSave;
     @FXML private TextField newQuoteSearch;
-    @FXML private TableColumn<?, ?> newQuoteSelectedTooth;
+    @FXML private TableColumn<Quote, Integer> newQuoteSelectedTooth;
+    Quote quote;
+
+    @FXML
+    public void initialized(){
+        //Add listener for PERSON and DATE of the new quote
+        newQuoteName.textProperty().addListener(((observable, oldValue, newValue) -> quote.getPerson().setFirstName(newValue)));
+        newQuoteLastName.textProperty().addListener(((observable, oldValue, newValue) -> quote.getPerson().setLastName(newValue)));
+        newQuoteDate.valueProperty().addListener(((observable, oldValue, newValue) -> quote.setQuoteDate(newValue)));
+
+        //Initialized the table of ALL services
+        newQuoteNameColumn.setCellValueFactory(new PropertyValueFactory<>("serviceName"));
+        newQuotePriceColumn.setCellValueFactory(new PropertyValueFactory<>("servicePrice"));
+        newQuotePriceForToothColumn.setCellValueFactory(new PropertyValueFactory<>("servicePriceForTooth"));
+
+        //Initialized the table of SELECTED services
+        newQuoteNameChosenColumn.setCellValueFactory(new PropertyValueFactory<>("serviceChosenName"));
+        newQuoteNumberColumn.setCellValueFactory(new PropertyValueFactory<>("serviceChosenNumber"));
+        newQuoteSelectedTooth.setCellValueFactory(new PropertyValueFactory<>("serviceChosenTooth"));
+
+        //Add listener for TABLE of ALL services
+
+
+        //Add listener for TABLE of SELECTED services
+
+
+    }
 }
