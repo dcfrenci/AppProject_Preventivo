919f0416c554575081bdaa2616eec20b30f91772
lorenzo <lorenzodimaio02@gmail.com>
2023-05-05 13:08:15 +0200

    mia versione

--- src/main/java/com/preventivoapp/appproject_preventivo/controller/serviceSettingController.java
+++ src/main/java/com/preventivoapp/appproject_preventivo/controller/serviceSettingController.java
@@ -1,4 +1,22 @@
 package com.preventivoapp.appproject_preventivo.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class serviceSettingController {
+    @FXML
+    private Button newServiceCancel;
+
+    @FXML
+    private TextField newServiceNameColumn;
+
+    @FXML
+    private TextField newServicePriceColumn;
+
+    @FXML
+    private TextField newServicePriceForToothColumn;
+
+    @FXML
+    private Button newServiceSave;
 }
