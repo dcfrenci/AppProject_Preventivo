6737df9d05c4db8fb039cf42571fa751c6a1007b
dcfrenci2 <dcfrenci2@gmail.com>
2023-06-30 17:03:59 +0200

    added css style to page

--- src/main/java/com/preventivoapp/appproject_preventivo/QuoteMainApplication.java
+++ src/main/java/com/preventivoapp/appproject_preventivo/QuoteMainApplication.java
@@ -1,11 +1,15 @@
 package com.preventivoapp.appproject_preventivo;
 
+import com.preventivoapp.appproject_preventivo.classes.Quote;
 import com.preventivoapp.appproject_preventivo.controller.quoteMainController;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
+import java.util.Objects;
 
 public class QuoteMainApplication extends Application {
 
@@ -14,6 +18,8 @@ public class QuoteMainApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(QuoteMainApplication.class.getResource("quoteMain-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         primaryStage.setTitle("Quote Maker");
+        //primaryStage.getIcons().add(new Image(Objects.requireNonNull(QuoteMainApplication.class.getResource("icons8-documento-64.png")).openStream()));
+        primaryStage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("icons8-documento-64.png"))));
         primaryStage.setScene(scene);
         primaryStage.setMaximized(true);
         primaryStage.show();
